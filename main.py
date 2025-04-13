597a85230e3d5b7bc3b2a40a16641c9c7a8b55ba
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-13 11:53:10 +0530

    Refactor argument parser setup

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from matplotlib import pyplot as plt
 from models import *
 from evaluation import *
 from preprocess import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", type=int, default=30000, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=128, help="size of the batches")
+parser.add_argument("--sample_interval", type=int, default=10, help="interval between image sampling")
+parser.add_argument("--z_dim", type=int, default=100, help="dimension of z sampler.")
+opt = parser.parse_args()

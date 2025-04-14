cf16178bdf94d7169777820dc843e56818c7384b
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-14 10:54:02 +0530

    Refactor evaluation class to include feature vector extraction and similarity calculation methods

--- evaluation.py
+++ evaluation.py
@@ -7,3 +7,13 @@ class evaluation():
 	def __init__(self):
 		vgg16 = VGG16(weights='imagenet')
 		self.model = Model(inputs=vgg16.input, outputs=vgg16.get_layer('fc2').output)
+		
+	def get_feature_vector(self, img):
+		img = cv2.resize(img, (224, 224))
+		feature_vector = self.model(img.reshape(1, 224, 224, 3))
+		return feature_vector
+	
+	def calculate_similarity(self, img1, img2):
+		vector1 = self.get_feature_vector(img1)
+		vector2 = self.get_feature_vector(img2)
+		return 1 - cosine(vector1, vector2)

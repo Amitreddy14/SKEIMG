9e4a72eb876a9b608f4ae57f2f21e4184acefc27
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-14 10:50:46 +0530

    Add ContextualGAN class with initialization parameters

--- models.py
+++ models.py
@@ -16,3 +16,13 @@ parser.add_argument("--batch_size", type=int, default=128, help="size of the bat
 parser.add_argument("--gf_dim", type=int, default=64, help="dimension of gen filters in first conv layer.")
 parser.add_argument("--df_dim", type=int, default=64, help="dimension of discrim filters in first conv layer.")
 opt = parser.parse_args()
+
+class ContextualGAN():
+    def __init__(self):
+        self.img_rows = 64
+        self.img_cols = 128
+        self.channels = 3
+        self.num_classes = 2
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+
+        self.optimizer = Adam(opt.lr, opt.b)

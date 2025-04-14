d1343c081e757d0c6baa1c3b3da731f01220ba93
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-14 10:52:51 +0530

    Fix formatting by adding a newline at the end of evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -2,3 +2,8 @@ import cv2
 from scipy.spatial.distance import cosine
 from keras.models import Model
 from tensorflow.keras.applications import VGG16
+
+class evaluation():
+	def __init__(self):
+		vgg16 = VGG16(weights='imagenet')
+		self.model = Model(inputs=vgg16.input, outputs=vgg16.get_layer('fc2').output)

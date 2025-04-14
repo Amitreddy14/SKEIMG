6bcf8494b9b3530377960c97bef46bd9578d5661
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-14 10:23:36 +0530

    Add alternate training function with loss tracking

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ parser.add_argument("--batch_size", type=int, default=128, help="size of the bat
 parser.add_argument("--sample_interval", type=int, default=10, help="interval between image sampling")
 parser.add_argument("--z_dim", type=int, default=100, help="dimension of z sampler.")
 opt = parser.parse_args()
+
+# Alternate training
+def alt_train(model, X_train):
+
+    d_loss_list = []
+    g_loss_list = []
+
+    # Adversarial ground truths
+    valid = np.ones((opt.batch_size, 1))
+    fake = np.zeros((opt.batch_size, 1))
+
+    X_train = X_train.astype('float32') / 255
+
+    for epoch in range(opt.epochs):

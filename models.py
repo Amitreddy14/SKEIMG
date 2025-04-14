d49d6c789a99637cb06f164488288b74a7015cec
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-14 10:51:15 +0530

    Add newline at the end of the ContextualGAN class definition

--- models.py
+++ models.py
@@ -26,3 +26,16 @@ class ContextualGAN():
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
 
         self.optimizer = Adam(opt.lr, opt.b)
+
+        def contextual_loss(y_true, y_pred):
+            y_true = tf.image.rgb_to_grayscale(tf.slice(y_true, [0,0,0,0], [opt.batch_size, self.img_rows, self.img_cols, self.channels]))
+            y_pred = tf.image.rgb_to_grayscale(tf.slice(y_pred, [0,0,0,0], [opt.batch_size, self.img_rows, self.img_cols, self.channels]))
+            
+            y_true = tf.divide(tf.add(tf.reshape(y_true, [tf.shape(y_true)[0], -1]), 1), 2)
+            y_pred = tf.divide(tf.add(tf.reshape(y_pred, [tf.shape(y_pred)[0], -1]), 1), 2)
+                
+            # normalize sum to 1
+            y_true = tf.divide(y_true, tf.tile(tf.expand_dims(tf.reduce_sum(y_true, axis=1), 1), [1,tf.shape(y_true)[1]]))
+            y_pred = tf.divide(y_pred, tf.tile(tf.expand_dims(tf.reduce_sum(y_pred, axis=1), 1), [1,tf.shape(y_pred)[1]]))
+
+            return tf.reduce_sum(tf.multiply(y_true, tf.math.log(tf.divide(y_true, y_pred))), axis=1)

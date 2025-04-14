e75f436da0760737e7c937920d1fb6a5e53d8e3c
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-14 10:57:34 +0530

    Add extract_classwise_instances function for object instance extraction

--- preprocess.py
+++ preprocess.py
@@ -21,3 +21,27 @@ def get_images_paths(directory_name, image_type='png'):
     end = "/*." + image_type
 
     return glob.glob(glob.escape(directory_name) + end)
+
+def extract_classwise_instances(samples, output_dir, label_field, size_lower_limit, ext=".png"):
+    print("Extracting object instances...")
+    for sample in samples.iter_samples(progress=True):
+        img = cv2.imread(sample.filepath)
+        img_h,img_w,c = img.shape
+        if img_h >= size_lower_limit and img_w >= size_lower_limit:
+            for det in sample[label_field].detections:
+                mask = det.mask
+                [x,y,w,h] = det.bounding_box
+                x = int(x * img_w)
+                y = int(y * img_h)
+                h, w = mask.shape
+                mask_img = img[y:y+h, x:x+w, :]
+                alpha = mask.astype(np.uint8)*255
+                alpha = np.expand_dims(alpha, 2)
+                mask_img = np.concatenate((mask_img, alpha), axis=2)
+                label = det.label
+                label_dir = os.path.join(output_dir, label)
+
+                if not os.path.exists(label_dir):
+                    os.mkdir(label_dir)
+                output_filepath = os.path.join(label_dir, det.id+ext)
+                cv2.imwrite(output_filepath, mask_img)

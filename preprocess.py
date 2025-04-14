9a24d9e6cc1dfac0f40373c8e3094e770750826e
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-14 10:56:48 +0530

    Fix formatting by adding newlines at the end of preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -6,3 +6,18 @@ import cv2
 import fiftyone as fo
 import fiftyone.zoo as foz
 from fiftyone import ViewField as F
+
+def get_images_paths(directory_name, image_type='png'):
+    """
+    get the file name/path of all the files within a folder.
+        e.g. glob.glob("/home/adam/*/*.txt").
+    Use glob.escape to escape strings that are not meant to be patterns
+        glob.glob(glob.escape(directory_name) + "/*.txt")
+    :param directory_name: (str) the root directory name that contains all the images we want
+    :param image: (str) either "jpg" or "png"
+    :return: a list of queried files and directories
+    """
+    # concatnate strings
+    end = "/*." + image_type
+
+    return glob.glob(glob.escape(directory_name) + end)
